07b661a2ce97a2309bf332ff1ab76d0e33fef868
adnan <adnan333bd@gmail.com>
2020-08-21 18:15:08 +0600

    fb project setup

--- fitness-app/src/app/app.module.ts
+++ fitness-app/src/app/app.module.ts
@@ -16,3 +16,18 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* var firebaseConfig = {
+  apiKey: "AIzaSyDPaqV3-sJ2IEF5JUhWON0KLtWvurJ3sIo",
+  authDomain: "fitness-app-775c0.firebaseapp.com",
+  databaseURL: "https://fitness-app-775c0.firebaseio.com",
+  projectId: "fitness-app-775c0",
+  storageBucket: "fitness-app-775c0.appspot.com",
+  messagingSenderId: "710654031385",
+  appId: "1:710654031385:web:352910baf2c92ecdc09c88",
+  measurementId: "G-3Q0BC7F9FL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+ */
